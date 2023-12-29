9608be233d7c9ae0f0ffd5752c455d83b5066c67
Ben Lu <ayroblu@gmail.com>
2023-12-29 12:56:15 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Link } from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+      <Link to="/todo">Go to todo</Link>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/todo",
+    element: <div>temp</div>,
+  },
+], {basename: import.meta.env.BASE_URL});
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
