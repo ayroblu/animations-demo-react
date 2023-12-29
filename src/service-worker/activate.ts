d654721f60256b68f107c0a4160cb736267a5a9e
Ben Lu <ayroblu@gmail.com>
2023-12-29 21:57:41 +0000

    Revert "disable preload temporarily"
    This reverts commit 5b6e14116ac0b65650c6d0f21af5b5f1db5a8e78.

--- src/service-worker/activate.ts
+++ src/service-worker/activate.ts
@@ -28,7 +28,7 @@ export function handleNavigationPreload(event: ExtendableEvent) {
 }
 async function setNavigationPreload() {
   if (self.registration.navigationPreload) {
-    if (process.env.NODE_ENV === "production2") {
+    if (process.env.NODE_ENV === "production") {
       await self.registration.navigationPreload.enable();
     } else {
       // In devel, we have no manifest, so can't identify precache
