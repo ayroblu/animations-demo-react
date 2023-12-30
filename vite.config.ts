45e0915083bc7a95ad238314be2ffd2c475fe9ee
Ben Lu <ayroblu@gmail.com>
2023-12-30 00:29:55 +0000

    remove scope change

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,6 @@ export default defineConfig({
       strategies: "injectManifest",
       srcDir: "src/service-worker",
       filename: "sw.ts",
-      scope: "./",
       // injectRegister: "inline",
       devOptions: {
         enabled: true,
