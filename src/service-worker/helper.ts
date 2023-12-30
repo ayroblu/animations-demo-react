d6055d0b995b995da72441f4e8b9c8e40ad63d78
Ben Lu <ayroblu@gmail.com>
2023-12-30 00:38:51 +0000

    fix paths

--- src/service-worker/helper.ts
+++ src/service-worker/helper.ts
@@ -1,8 +1,13 @@
+const base = import.meta.env.BASE_URL;
+
 declare const self: ServiceWorkerGlobalScope;
 
 function getCacheablePaths() {
   const manifest = self.__WB_MANIFEST ?? [];
-  return manifest;
+  return manifest.map(({ url, revision }) => ({
+    url: `${base}${url}`,
+    revision,
+  }));
 }
 
 // if you ever change this, you need to delete existing cache storage
